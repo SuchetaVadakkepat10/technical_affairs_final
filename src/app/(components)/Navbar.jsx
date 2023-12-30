c0ab1091fed4cac56080ed956ee19337be3121fc
harshithb3304 <harsh03032004@gmail.com>
2023-12-30 20:26:27 +0530

    not fixed image slider added joy updated clubs

--- src/app/(components)/Navbar.jsx
+++ src/app/(components)/Navbar.jsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
-const Navbar = () =>{
-  const[isClick, setisClick] = useState(false);
+const Navbar = () => {
+  const [isClick, setisClick] = useState(false);
 
-  const toggleNavbar = () =>{
+  const toggleNavbar = () => {
     setisClick(!isClick);
   };
   return (
-  <>
-    <div className="bg-black">
+    <>
+      <div className="bg-black">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
@@ -20,42 +20,70 @@ const Navbar = () =>{
               </div>
             </div>
             <div className="hidden md:block">
-                <div className="al-4 flex items-center space-x-4">
-                  <a href="/home" className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    Home
-                  </a>
+              <div className="al-4 flex items-center space-x-4">
+                <a
+                  href="/home"
+                  className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2"
+                >
+                  Home
+                </a>
 
-                  <a href="/clubs" className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    Clubs
-                  </a>
+                <a
+                  href="/clubs"
+                  className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2"
+                >
+                  Clubs
+                </a>
 
-                  <a href="/about" className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    About
-                  </a>
-                  <a href="/" className="text-white hover:bg-blue-200 hover:text-white rounded-lg p-2">
-                    Joy of giving
-                  </a>
-                </div>
+                <a
+                  href="/about"
+                  className="text-white hover:bg-blue-200 hover:text-black rounded-lg p-2"
+                >
+                  About
+                </a>
+                <a
+                  href="/joy"
+                  className="text-white hover:bg-blue-200 hover:text-white rounded-lg p-2"
+                >
+                  Joy of giving
+                </a>
+              </div>
             </div>
             <div className="md:hidden flex items-center">
-              <button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"onClick={toggleNavbar}>
+              <button
+                className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={toggleNavbar}
+              >
                 {isClick ? (
                   <svg
-                  className= "h-6 w-6"
+                    className="h-6 w-6"
                     xmlns="https://www.w3.org/2000/svg"
-                    fill='none'
+                    fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>                  </svg>
-                ): (<svg className= "h-6 w-6"
-                xmlns="https://www.w3.org/2000/svg"
-                fill='none'
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"/>
-              </svg>
-
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />{" "}
+                  </svg>
+                ) : (
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="https://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
                 )}
               </button>
             </div>
@@ -64,25 +92,37 @@ const Navbar = () =>{
         {isClick && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="/home" className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    Home
-                  </a>
+              <a
+                href="/home"
+                className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2"
+              >
+                Home
+              </a>
 
-                  <a href="/clubs" className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    Clubs
-                  </a>
+              <a
+                href="/clubs"
+                className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2"
+              >
+                Clubs
+              </a>
 
-                  <a href="/about" className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2">
-                    About
-                  </a>
-                  <a href="/" className="text-white block hover:bg-blue-200 hover:text-white rounded-lg p-2">
-                    Joy of giving
-                  </a>
+              <a
+                href="/about"
+                className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2"
+              >
+                About
+              </a>
+              <a
+                href="/joy"
+                className="text-white block hover:bg-blue-200 hover:text-black rounded-lg p-2"
+              >
+                Joy of giving
+              </a>
             </div>
           </div>
         )}
-    </div>
-  </>
+      </div>
+    </>
   );
 };
 
---
+++ src/app/joy/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../(components)/Footer";
+
+const page = () => {
+  return (
+    <main>
+      <div className="flex items-center justify-center h-screen bg-blue-200 text-[40px]">
+        COMING SOON!
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+export default page;
