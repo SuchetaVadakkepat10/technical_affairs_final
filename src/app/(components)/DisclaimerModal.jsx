af38b14955bdb221c114b70302f25bd4feccdf4f
harshithb3304 <harsh03032004@gmail.com>
2024-01-01 18:22:53 +0530

    Slider + Footer+ DisclaimerModal

--- src/app/(components)/DisclaimerModal.jsx
+++ src/app/(components)/DisclaimerModal.jsx
@@ -41,26 +41,26 @@ const DisclaimerModal = ({ isOpen, onClose }) => {
           <h2 className="text-2xl font-bold mb-2">Content Disclaimer</h2>
           <hr className="border-t border-white w-full" />
           <p>
-            All information provided in the "Technical Affairs IIITDM
-            Kancheepuram" hereafter referred to as "Tech Affairs", Website is
-            provided for information purposes only and does not constitute a
-            legal contract between Tech Affairs and any person or entity unless
-            otherwise specified. Information on official Tech Affairs websites
-            is subject to change without prior notice. Although every reasonable
-            effort is made to present current and accurate information, Tech
-            Affairs makes no guarantees of any kind. The Tech Affairs website
-            may contain information that is created and maintained by a variety
-            of sources both internal and external to the Institute. These sites
-            are unmoderated forums containing the personal opinions and other
-            expressions of the persons who post the entries. Tech Affairs does
-            not control, monitor, or guarantee the information contained in
-            these sites or information contained in links to other external
-            websites, and does not endorse any views expressed or products or
-            services offered therein. In no event shall tech Affairs be
-            responsible or liable, directly or indirectly, for any damage or
-            loss caused or alleged to be caused by or in connection with the use
-            of or reliance on any such content, goods, or services available on
-            or through any such site or resource.
+            All information provided in the &quot;Technical Affairs IIITDM
+            Kancheepuram&quot; hereafter referred to as &quot;Tech
+            Affairs&quot;, Website is provided for information purposes only and
+            does not constitute a legal contract between Tech Affairs and any
+            person or entity unless otherwise specified. Information on official
+            Tech Affairs websites is subject to change without prior notice.
+            Although every reasonable effort is made to present current and
+            accurate information, Tech Affairs makes no guarantees of any kind.
+            The Tech Affairs website may contain information that is created and
+            maintained by a variety of sources both internal and external to the
+            Institute. These sites are unmoderated forums containing the
+            personal opinions and other expressions of the persons who post the
+            entries. Tech Affairs does not control, monitor, or guarantee the
+            information contained in these sites or information contained in
+            links to other external websites, and does not endorse any views
+            expressed or products or services offered therein. In no event shall
+            Tech Affairs be responsible or liable, directly or indirectly, for
+            any damage or loss caused or alleged to be caused by or in
+            connection with the use of or reliance on any such content, goods,
+            or services available on or through any such site or resource.
           </p>
           <h2 className="text-2xl font-bold mb-2">Link Disclaimer</h2>
           <hr className="border-t border-white w-full" />
